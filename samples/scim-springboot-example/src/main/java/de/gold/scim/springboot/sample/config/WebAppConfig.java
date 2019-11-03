cffdf844e7b15009fe7cafd3b6e308985c065b02
Pascal Knueppel <captain.p.goldfish@gmx.de>
2019-11-03 11:32:55 +0100

    Add a sample project with springboot

---
+++ samples/scim-springboot-example/src/main/java/de/gold/scim/springboot/sample/config/WebAppConfig.java
@@ -0,0 +1,84 @@
+package de.gold.scim.springboot.sample.config;
+
+import java.util.Collections;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import de.gold.scim.common.resources.ServiceProvider;
+import de.gold.scim.common.resources.complex.AuthenticationScheme;
+import de.gold.scim.common.resources.complex.BulkConfig;
+import de.gold.scim.common.resources.complex.ChangePasswordConfig;
+import de.gold.scim.common.resources.complex.FilterConfig;
+import de.gold.scim.common.resources.complex.PatchConfig;
+import de.gold.scim.common.resources.complex.SortConfig;
+import de.gold.scim.server.endpoints.ResourceEndpoint;
+import de.gold.scim.server.endpoints.base.UserEndpointDefinition;
+import de.gold.scim.server.schemas.ResourceType;
+import de.gold.scim.springboot.sample.handler.UserHandler;
+
+
+/**
+ * author Pascal Knueppel <br>
+ * created at: 02.11.2019 - 23:58 <br>
+ * <br>
+ */
+@Configuration
+@EnableWebMvc
+@ComponentScan("de.gold.scim.springboot.sample")
+public class WebAppConfig
+{
+
+  /**
+   * create the service provider configuration
+   */
+  @Bean
+  public ServiceProvider getServiceProviderConfig()
+  {
+    AuthenticationScheme authScheme = AuthenticationScheme.builder()
+                                                          .name("OAuth Bearer Token")
+                                                          .description("Authentication scheme using the OAuth "
+                                                                       + "Bearer Token Standard")
+                                                          .specUri("http://www.rfc-editor.org/info/rfc6750")
+                                                          .type("oauthbearertoken")
+                                                          .build();
+    return ServiceProvider.builder()
+                          .filterConfig(FilterConfig.builder().supported(true).maxResults(50).build())
+                          .sortConfig(SortConfig.builder().supported(true).build())
+                          .changePasswordConfig(ChangePasswordConfig.builder().supported(true).build())
+                          .bulkConfig(BulkConfig.builder().supported(true).maxOperations(10).build())
+                          .patchConfig(PatchConfig.builder().supported(true).build())
+                          .authenticationSchemes(Collections.singletonList(authScheme))
+                          .build();
+  }
+
+  /**
+   * creates a resource endpoint for scim
+   *
+   * @param serviceProvider the service provider configuration
+   * @return the resource endpoint
+   */
+  @Bean
+  public ResourceEndpoint getResourceEndpoint(ServiceProvider serviceProvider)
+  {
+    return new ResourceEndpoint(serviceProvider);
+  }
+
+  /**
+   * gets the user resource type that is the core object of the /Users endpoint. We will also activate the
+   * auto-filtering extension so that filtering will work on the fly
+   *
+   * @param resourceEndpoint the resource endpoint that was previously defined
+   * @return the user resource type
+   */
+  @Bean
+  public ResourceType getUserResourceType(ResourceEndpoint resourceEndpoint)
+  {
+    ResourceType userResourceType = resourceEndpoint.registerEndpoint(new UserEndpointDefinition(new UserHandler()));
+    userResourceType.setFilterExtension(new ResourceType.FilterExtension(true));
+    return userResourceType;
+  }
+
+}
---
+++ samples/scim-springboot-example/src/main/java/de/gold/scim/springboot/sample/handler/UserHandler.java
@@ -0,0 +1,148 @@
+package de.gold.scim.springboot.sample.handler;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+import de.gold.scim.common.constants.ResourceTypeNames;
+import de.gold.scim.common.constants.enums.SortOrder;
+import de.gold.scim.common.exceptions.ConflictException;
+import de.gold.scim.common.exceptions.ResourceNotFoundException;
+import de.gold.scim.common.resources.User;
+import de.gold.scim.common.resources.complex.Meta;
+import de.gold.scim.common.schemas.SchemaAttribute;
+import de.gold.scim.server.endpoints.ResourceHandler;
+import de.gold.scim.server.filter.FilterNode;
+import de.gold.scim.server.response.PartialListResponse;
+import lombok.Getter;
+
+
+/**
+ * author Pascal Knueppel <br>
+ * created at: 03.11.2019 - 00:14 <br>
+ * <br>
+ */
+public class UserHandler extends ResourceHandler<User>
+{
+
+  /**
+   * an in memory map that holds our 5000 users
+   */
+  @Getter
+  private Map<String, User> inMemoryMap = new HashMap<>();
+
+  /**
+   * adds approximately 5000 users into the in memory map
+   */
+  public UserHandler()
+  {
+    try (InputStream inputStream = getClass().getResourceAsStream("/firstnames.txt");
+      InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+      BufferedReader reader = new BufferedReader(inputStreamReader))
+    {
+
+      String name;
+      while ((name = reader.readLine()) != null)
+      {
+        String id = UUID.randomUUID().toString();
+        Meta meta = Meta.builder()
+                        .created(LocalDateTime.now())
+                        .lastModified(LocalDateTime.now())
+                        .resourceType(ResourceTypeNames.USER)
+                        .build();
+        inMemoryMap.put(id, User.builder().id(id).userName(name).nickName(name).meta(meta).build());
+      }
+    }
+    catch (IOException e)
+    {
+      throw new IllegalStateException(e.getMessage(), e);
+    }
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public User createResource(User resource)
+  {
+    final String userId = UUID.randomUUID().toString();
+    if (inMemoryMap.containsKey(userId))
+    {
+      throw new ConflictException("resource with id '" + userId + "' does already exist");
+    }
+    resource.setId(userId);
+    inMemoryMap.put(userId, resource);
+    resource.getMeta().ifPresent(meta -> {
+      meta.setCreated(Instant.now());
+      meta.setLastModified(Instant.now());
+    });
+    return resource;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public User getResource(String id)
+  {
+    return inMemoryMap.get(id);
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public PartialListResponse<User> listResources(long startIndex,
+                                                 int count,
+                                                 FilterNode filter,
+                                                 SchemaAttribute sortBy,
+                                                 SortOrder sortOrder)
+  {
+    List<User> resourceNodes = new ArrayList<>(inMemoryMap.values());
+    return PartialListResponse.<User> builder().resources(resourceNodes).totalResults(resourceNodes.size()).build();
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public User updateResource(User resource)
+  {
+    String userId = resource.getId().get();
+    User oldUser = inMemoryMap.get(userId);
+    if (oldUser == null)
+    {
+      throw new ResourceNotFoundException("resource with id '" + userId + "' does not exist", null, null);
+    }
+    resource.getMeta().get().setCreated(oldUser.getMeta().get().getCreated().get());
+    inMemoryMap.put(userId, resource);
+    resource.getMeta().ifPresent(meta -> {
+      meta.setLastModified(Instant.now());
+    });
+    return resource;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void deleteResource(String id)
+  {
+    if (inMemoryMap.containsKey(id))
+    {
+      inMemoryMap.remove(id);
+    }
+    else
+    {
+      throw new ResourceNotFoundException("resource with id '" + id + "' does not exist", null, null);
+    }
+  }
+}
