88c5fb711ed984095a824098078df2d779d52d81
Pascal Knüppel <captain.p.goldfish@gmx.de>
2020-05-15 22:13:21 +0200

    Add missing noArgsConstructors

--- scim-sdk-common/src/main/java/de/captaingoldfish/scim/sdk/common/resources/complex/FilterConfig.java
+++ scim-sdk-common/src/main/java/de/captaingoldfish/scim/sdk/common/resources/complex/FilterConfig.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import de.captaingoldfish.scim.sdk.common.constants.AttributeNames;
 import de.captaingoldfish.scim.sdk.common.resources.base.ScimObjectNode;
 import lombok.Builder;
+import lombok.NoArgsConstructor;
 
 
 /**
@@ -13,6 +14,7 @@ import lombok.Builder;
  * <br>
  * A complex type that specifies FILTER options. REQUIRED. See Section 3.4.2.2 of [RFC7644].
  */
+@NoArgsConstructor
 public class FilterConfig extends ScimObjectNode
 {
 
