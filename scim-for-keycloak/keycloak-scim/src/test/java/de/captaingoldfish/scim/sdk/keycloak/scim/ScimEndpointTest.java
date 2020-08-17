02a45ff1209ac7bd63a6dfd063bcacf559178fbb
Pascal Knüppel <captain.p.goldfish@gmx.de>
2020-08-17 21:28:43 +0200

    Fix unit test

--- scim-for-keycloak/keycloak-scim/src/test/java/de/captaingoldfish/scim/sdk/keycloak/scim/ScimEndpointTest.java
+++ scim-for-keycloak/keycloak-scim/src/test/java/de/captaingoldfish/scim/sdk/keycloak/scim/ScimEndpointTest.java
@@ -57,7 +57,7 @@ public class ScimEndpointTest extends KeycloakScimManagementTest
     // the endpoint is already initialized by method super.initializeEndpoint()
     new ScimEndpoint(getKeycloakSession(), getAuthentication());
     Assertions.assertEquals(1, countEntriesInTable(ScimServiceProviderEntity.class));
-    Assertions.assertEquals(2, countEntriesInTable(ScimResourceTypeEntity.class));
+    Assertions.assertEquals(3, countEntriesInTable(ScimResourceTypeEntity.class));
   }
 
   /**
