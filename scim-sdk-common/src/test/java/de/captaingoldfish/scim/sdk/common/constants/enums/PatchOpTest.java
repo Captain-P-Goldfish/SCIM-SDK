45e11d5df5febe5f3a531c2d00033e5420e680c5
Pascal Knüppel <captain.p.goldfish@gmx.de>
2021-03-11 20:55:38 +0100

    Fix comparison of patch operations

--- scim-sdk-common/src/test/java/de/captaingoldfish/scim/sdk/common/constants/enums/PatchOpTest.java
+++ scim-sdk-common/src/test/java/de/captaingoldfish/scim/sdk/common/constants/enums/PatchOpTest.java
@@ -2,6 +2,8 @@ package de.captaingoldfish.scim.sdk.common.constants.enums;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import de.captaingoldfish.scim.sdk.common.exceptions.UnknownValueException;
 
@@ -20,5 +22,15 @@ public class PatchOpTest
     Assertions.assertThrows(UnknownValueException.class, () -> PatchOp.getByValue(" "));
     Assertions.assertThrows(UnknownValueException.class, () -> PatchOp.getByValue(null));
     Assertions.assertThrows(UnknownValueException.class, () -> PatchOp.getByValue("patch"));
+  }
+
+  /**
+   * verifies that the patchop value can be retrieved case insensitive
+   */
+  @ParameterizedTest
+  @ValueSource(strings = {"add", "ADD", "AdD", "replace", "REPLACE", "rePlacE", "remove", "REMOVE", "rEmOve"})
+  public void testgetPatchOpByValueIsCaseInsensitive(String patchOpValue)
+  {
+    Assertions.assertDoesNotThrow(() -> PatchOp.getByValue(patchOpValue));
   }
 }
