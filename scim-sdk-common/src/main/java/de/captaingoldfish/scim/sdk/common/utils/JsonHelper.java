915f327f1c0b1eb3707b9aec6eef51db24aad4cb
Pascal Knüppel <captain.p.goldfish@gmx.de>
2021-04-19 18:48:32 +0200

    Revert "Remove unused type from parsing conditions"
    This reverts commit 9f729101

--- scim-sdk-common/src/main/java/de/captaingoldfish/scim/sdk/common/utils/JsonHelper.java
+++ scim-sdk-common/src/main/java/de/captaingoldfish/scim/sdk/common/utils/JsonHelper.java
@@ -410,6 +410,10 @@ public final class JsonHelper
     {
       return Optional.of((T)Long.valueOf(attribute.asLong()));
     }
+    if (Float.class.equals(type))
+    {
+      return Optional.of((T)Float.valueOf((float)attribute.asDouble()));
+    }
     if (Double.class.equals(type))
     {
       return Optional.of((T)Double.valueOf(attribute.asDouble()));
