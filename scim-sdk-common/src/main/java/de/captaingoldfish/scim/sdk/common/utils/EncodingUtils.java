45373808bcf2a710e02341b9225d25db8a8e6496
Michael Berning <mberning@gmail.com>
2023-03-24 15:56:33 -0400

    Add EncodingUtils class and accompanying test case
    Fixes #452

---
+++ scim-sdk-common/src/main/java/de/captaingoldfish/scim/sdk/common/utils/EncodingUtils.java
@@ -0,0 +1,33 @@
+package de.captaingoldfish.scim.sdk.common.utils;
+
+import de.captaingoldfish.scim.sdk.common.exceptions.InternalServerException;
+import org.apache.commons.lang3.StringUtils;
+
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
+
+/**
+ * This class provides methods for encoding data into specific formats.
+ */
+public class EncodingUtils
+{
+
+  /**
+   * This method takes the input string and URL Encodes it using java.net.URLEncoder
+   *
+   * @param value The value to be URL encoded.
+   * @return A URL encoded version of the input value
+   */
+  public static String urlEncode(String value)
+  {
+    try
+    {
+      return StringUtils.isBlank(value) ? "" : URLEncoder.encode(value, StandardCharsets.UTF_8.toString());
+    }
+    catch (Exception e)
+    {
+      throw new InternalServerException(e.getMessage(), e);
+    }
+  }
+}
