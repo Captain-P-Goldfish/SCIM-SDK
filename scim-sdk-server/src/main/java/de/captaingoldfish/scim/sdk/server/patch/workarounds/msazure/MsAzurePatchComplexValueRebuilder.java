83aac86807948759a8c62728568b54d5dae8c307
Vojtech Pustovka <144007640+vojtechpustovka@users.noreply.github.com>
2024-07-17 15:43:12 +0200

    MsAzurePatchComplexValueRebuilder now accepts various simple strings (#691)
    * MsAzurePatchComplexValueRebuilder now accepts various simple strings
    
    * Applied maven formatter
    
    * Refactored code and added docs to improve readability

--- scim-sdk-server/src/main/java/de/captaingoldfish/scim/sdk/server/patch/workarounds/msazure/MsAzurePatchComplexValueRebuilder.java
+++ scim-sdk-server/src/main/java/de/captaingoldfish/scim/sdk/server/patch/workarounds/msazure/MsAzurePatchComplexValueRebuilder.java
@@ -2,6 +2,10 @@ package de.captaingoldfish.scim.sdk.server.patch.workarounds.msazure;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.commons.lang3.StringUtils;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ArrayNode;
@@ -10,6 +14,7 @@ import com.fasterxml.jackson.databind.node.TextNode;
 
 import de.captaingoldfish.scim.sdk.common.constants.AttributeNames;
 import de.captaingoldfish.scim.sdk.common.constants.enums.Type;
+import de.captaingoldfish.scim.sdk.common.exceptions.IOException;
 import de.captaingoldfish.scim.sdk.common.request.PatchRequestOperation;
 import de.captaingoldfish.scim.sdk.common.resources.base.ScimObjectNode;
 import de.captaingoldfish.scim.sdk.common.resources.complex.PatchConfig;
@@ -67,6 +72,8 @@ import lombok.extern.slf4j.Slf4j;
 public class MsAzurePatchComplexValueRebuilder extends PatchWorkaround
 {
 
+  private static final String JSON_RESERVED_CHARS_PATTERN = "[\\[\\]{}:,]";
+
   private SchemaAttribute schemaAttribute;
 
   @Override
@@ -123,26 +130,12 @@ public class MsAzurePatchComplexValueRebuilder extends PatchWorkaround
   {
     List<String> patchValues = operation.getValues();
     List<String> fixedValues = new ArrayList<>();
-    for ( int i = 0 ; i < patchValues.size() ; i++ )
+    for ( String patchValue : patchValues )
     {
-      String patchValue = patchValues.get(i);
-      JsonNode jsonNode;
-      try
+      if (!shouldBeFixed(patchValue))
       {
-        jsonNode = JsonHelper.readJsonDocument(patchValue);
-      }
-      catch (Exception ex)
-      {
-        log.trace("[MS Azure complex-patch-path-value workaround] ignored value-node because it is no valid JSON "
-                  + "object-node");
-        fixedValues.add(patchValue);
-        continue;
-      }
-
-      if (jsonNode != null && (jsonNode.isObject() || jsonNode.isArray()))
-      {
-        log.trace("[MS Azure complex-patch-path-value workaround] ignoring non-simple-value for attribute {}",
-                  schemaAttribute.getScimNodeName());
+        log.trace("[MS Azure complex-patch-path-value workaround] ignored value-node because it is already in the correct form"
+                  + " or cannot be fixed");
         fixedValues.add(patchValue);
         continue;
       }
@@ -156,5 +149,46 @@ public class MsAzurePatchComplexValueRebuilder extends PatchWorkaround
 
     operation.setValues(fixedValues);
     return operation;
+  }
+
+  /**
+   * Decides whether the input JSON document is in an incorrect form and needs to be fixed. For input to be
+   * fixable, it must be a valid JSON in one of the following formats: blank, a simple string, a simple number,
+   * an object, or an array.
+   *
+   * @param jsonDocument the input JSON document to check
+   * @return true when the input JSON document needs to be fixed
+   */
+  private boolean shouldBeFixed(String jsonDocument)
+  {
+    if (StringUtils.isBlank(jsonDocument))
+    {
+      return true;
+    }
+
+    Pattern pattern = Pattern.compile(JSON_RESERVED_CHARS_PATTERN);
+    Matcher matcher = pattern.matcher(jsonDocument);
+    boolean isInputSimpleString = !matcher.find();
+
+    if (isInputSimpleString)
+    {
+      // Wraps input in double quotes to make it a valid json
+      jsonDocument = wrapInDoubleQuotes(jsonDocument);
+    }
+
+    try
+    {
+      JsonNode jsonNode = JsonHelper.readJsonDocument(jsonDocument);
+      return jsonNode != null && !jsonNode.isObject() && !jsonNode.isArray();
+    }
+    catch (IOException ex)
+    {
+      return false;
+    }
+  }
+
+  private String wrapInDoubleQuotes(String jsonDocument)
+  {
+    return String.format("\"%s\"", jsonDocument);
   }
 }
